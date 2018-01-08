b59718622e58b4561d2617a4657f76f8752768e4
Mac <mac@Macui-MacBook-Pro.local>
2018-01-08 10:19:33 +0900

    #1-32 Creating the URLs and testing the Image Serializers

--- nomadgram/images/urls.py
+++ nomadgram/images/urls.py
@@ -2,4 +2,9 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
+    url(
+        regex=r'all/$',
+        view=views.ListAllImages.as_view(),
+        name='all_images'
+    )
 ]
