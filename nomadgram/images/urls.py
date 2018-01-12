7b5a23ddf861ba450fc6ccece0dc1887acefe60a
Andy Woo <cheese.wo@gmail.com>
2018-01-12 22:42:06 +0900

    urls.py test

--- nomadgram/images/urls.py
+++ nomadgram/images/urls.py
@@ -7,5 +7,21 @@ urlpatterns = [
         view=views.Feed.as_view(),
         name='feed'
     ),
+    url(
+        regex=r'(?P<image_id>[0-9]+)/like/',
+        view=views.LikeImage.as_view(),
+        name='like_image'
+    )
 
 ]
+
+
+
+
+
+# /images/3/like
+
+
+#1 take the id from the url
+#2 we want to find an image with this id
+#3 we want to create a like for that image
