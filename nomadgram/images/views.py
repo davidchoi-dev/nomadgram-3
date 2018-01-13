3fdccfbe1e01616a8904e6d1d1388fa972602d69
Andy Woo <cheese.wo@gmail.com>
2018-01-13 19:25:21 +0900

    #1-45 Commenting on an Image

--- nomadgram/images/views.py
+++ nomadgram/images/views.py
@@ -69,13 +69,20 @@ class CommentOnImage(APIView):
 
     def post(self, request, image_id, format=None):
 
+        user = request.user
+
+        try:
+            found_image = models.Image.objects.get(id=image_id)
+        except models.Image.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
         serializer = serializers.CommentSerializer(data=request.data)
 
         if serializer.is_valid():
 
-            serializer.save()
+            serializer.save(creator=user, image=found_image)
 
-            return Response(data=serializer.data, status=status.HTTP_201_CREATED)
+            return Response(data=serializer.data,status=status.HTTP_201_CREATED)
 
         else:
 
