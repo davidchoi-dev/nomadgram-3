fe602978e67458e70817aaa1782ca1c7e64be217
Andy Woo <cheese.wo@gmail.com>
2018-01-14 15:48:36 +0900

    #1-61 Moderating comments on my image

--- nomadgram/images/urls.py
+++ nomadgram/images/urls.py
@@ -17,7 +17,12 @@ urlpatterns = [
         view=views.UnLikeImage.as_view(),
         name='like_image'
     ),
-      url(
+    url(
+        regex=r'^(?P<image_id>[0-9]+)/comments/(?P<comment_id>[0-9]+)/$',
+        view=views.ModerateComments.as_view(),
+        name='comment_image'
+    ),
+    url(
         regex=r'^(?P<image_id>[0-9]+)/comments/$',
         view=views.CommentOnImage.as_view(),
         name='comment_image'
