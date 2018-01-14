fd7d29715b62e727859ddfec5821a6d7db0bcfa1
Andy Woo <cheese.wo@gmail.com>
2018-01-14 18:09:48 +0900

    #1-66 Deleting an Image

--- nomadgram/images/urls.py
+++ nomadgram/images/urls.py
@@ -10,7 +10,7 @@ urlpatterns = [
     url(
         regex=r'^(?P<image_id>[0-9]+)/$',
         view=views.ImageDetail.as_view(),
-        name='like_detail'
+        name='image_detail'
     ),
     url(
         regex=r'^(?P<image_id>[0-9]+)/likes/$',
--- nomadgram/images/views.py
+++ nomadgram/images/views.py
@@ -203,6 +203,14 @@ class ModerateComments(APIView):
 
 
 class ImageDetail(APIView):
+
+    def find_own_image(self, image_id, user):
+        try:
+            image = models.Image.objects.get(id=image_id, creator=user)
+            return image
+        except models.Image.DoesNotExist:
+            return None
+
     def get(self, request, image_id, format=None):
 
         user = request.user
@@ -212,8 +220,8 @@ class ImageDetail(APIView):
         except models.Image.DoesNotExist:
             return Response(status=status.HTTP_404_NOT_FOUND)
 
-
-        serializer = serializers.ImageSerializer(image)
+        serializer = serializers.ImageSerializer(
+            image)
 
         return Response(data=serializer.data, status=status.HTTP_200_OK)
 
@@ -221,12 +229,11 @@ class ImageDetail(APIView):
 
         user = request.user
 
-        try:
-            image = models.Image.objects.get(id=image_id, creator=user)
+        image = self.find_own_image(image_id, user)
 
-        except models.Image.DoesNotExist:
+        if image is None:
+
             return Response(status=status.HTTP_401_UNAUTHORIZED)
-
 
         serializer = serializers.InputImageSerializer(
             image, data=request.data, partial=True)
@@ -240,5 +247,15 @@ class ImageDetail(APIView):
         else:
             return Response(data=serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+    def delete(self, request, image_id, format=None):
 
+        user = request.user
 
+        image = self.find_own_image(image_id, user)
+
+        if image is None:
+            return Response(status=status.HTTP_401_UNAUTHORIZED)
+
+        image.delete()
+
+        return Response(status=status.HTTP_204_NO_CONTENT)
