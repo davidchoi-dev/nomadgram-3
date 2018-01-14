677cba15b233d8b46edf37d5df7b85fbc12a9111
Andy Woo <cheese.wo@gmail.com>
2018-01-14 09:06:33 +0900

    Revert "error"
    This reverts commit 23c15de7aff980bb9f6bc226231d94090632dff7.

--- nomadgram/users/views.py
+++ nomadgram/users/views.py
@@ -109,17 +109,19 @@ class Search(APIView):
 
     def get(self, request, format=None):
 
-        username = request.query_params.get('username',None)
+        username = request.query_params.get('username', None)
 
-        if username is None:
+        if username is not None:
 
-            users = models.User.objects.filter(username__icontains=username)
+            users = models.User.objects.filter(username__istartswith=username)
 
-            serializer = serializers.ListUserSerializer(users, many=True)
-        
+            serializer = serializers.ListUserSerializer(
+                users, many=True, context={"request": request})
+
             return Response(data=serializer.data, status=status.HTTP_200_OK)
 
         else:
+
             return Response(status=status.HTTP_400_BAD_REQUEST)
 
 '''
