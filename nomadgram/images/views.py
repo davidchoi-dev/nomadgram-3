adac5d2cf014c3fa955aa06f9257ee0bb07cbbeb
Andy Woo <cheese.wo@gmail.com>
2018-01-14 23:54:47 +0900

    didn't post localhost/images

--- nomadgram/images/views.py
+++ nomadgram/images/views.py
@@ -6,7 +6,7 @@ from . import models, serializers
 from nomadgram.notifications import views as notification_views
 from nomadgram.users import serializers as user_serializers
 
-class Feed(APIView):
+class Images(APIView):
 
     def get(self, request, format=None):
 
@@ -36,6 +36,20 @@ class Feed(APIView):
         serializer = serializers.ImageSerializer(sorted_list, many=True)
 
         return Response(serializer.data)
+
+
+    def post(self, request,format=None):
+
+        user = request.user
+
+        serializer = serializers.InputImageSerializer(data=request.data)
+
+        if serializer.is_valid():
+            serializer.save(creator=user)
+            return Response(data=data.serializer.data, status=status.HTTP_201_CREATED)
+
+        else:
+            return Response(data=data.serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 
