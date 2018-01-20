7d948ce37039c0a1bf00f697a7791453739df2b7
Andy Woo <cheese.wo@gmail.com>
2018-01-20 15:40:47 +0900

    install redux

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import './index.css';
+import store from "./redux/configureStore"
 import App from './App';
 
 
