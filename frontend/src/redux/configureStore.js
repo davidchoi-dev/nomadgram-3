5ba516ee309674312647357e2eb466c25d3efee3
Andy Woo <cheese.wo@gmail.com>
2018-01-25 16:53:16 +0900

    fix confixgureStore.js error

--- frontend/src/redux/configureStore.js
+++ frontend/src/redux/configureStore.js
@@ -4,13 +4,17 @@ import users from "redux/modules/users";
 
 const middelwares = [thunk];
 
+const env = process;
+
+console.log(env);
+
 const reducer = combineReducers({
   users
 });
 
 
 let store = initialState => 
-  createStore(reducer, applyMiddleware(...middlewares));
+  createStore(reducer, applyMiddleware(...middelwares));
 
 
 export default store();
