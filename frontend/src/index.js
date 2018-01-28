2b49a645cf02d3df7171941690ab856961def19a
Andy Woo <cheese.wo@gmail.com>
2018-01-28 13:26:40 +0900

    #3-11 Syncing React Router with Redux

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -7,6 +7,7 @@ import store, { history } from "redux/configureStore";
 import "./index.css";
 import App from "./App";
 
+
 ReactDOM.render(
   <Provider store={store}>
     <ConnectedRouter history={history}>
