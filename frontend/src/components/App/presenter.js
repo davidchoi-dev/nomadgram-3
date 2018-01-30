0be1d61703bbd5385899b743967709e675c57390
Andy Woo <cheese.wo@gmail.com>
2018-01-31 08:09:56 +0900

    which?

--- frontend/src/components/App/presenter.js
+++ frontend/src/components/App/presenter.js
@@ -1,27 +1,30 @@
 import React from "react";
-import { Route,Switch } from "react-router-dom";
-import styles from "./styles.scss";
-import Footer from  "components/Footer";
+import PropTypes from "prop-types";
+import { Route, Switch } from "react-router-dom";
+import "./styles.scss";
+import Footer from "components/Footer";
 
 const App = props => [
-    // Nav,
-    props.isLoggedIn ? <PrivateRoutes key={2} /> : <PublicRoutes />,
-    <Footer key={3} />
-]
+  //Nav,
+  props.isLoggedIn ? <PrivateRoutes key={2} /> : <PublicRoutes key={2} />,
+  <Footer key={3} />
+];
 
+App.propTypes = {
+  isLoggedIn: PropTypes.bool.isRequired
+};
 
 const PrivateRoutes = props => (
-    <Switch>
-        <Route exact path="/" render={()=> "feed"} />
-       < Route exact path="/explore" render={() => "explore"} />
-    </Switch>
-)
+  <Switch>
+    <Route key="1" exact path="/" render={() => "feed"} />,
+    <Route key="2" path="explore" render={() => "explore"} />
+  </Switch>
+);
 
 const PublicRoutes = props => (
-    <Switch>
-        <Route exact path="/" render={()=> "login"} />
-       < Route exact path="/forgot" render={() => "password"} />
-    </Switch>
+  <Switch>
+    <Route path="/recover" render={() => "recover password"} />
+  </Switch>
+);
 
-)
 export default App;
