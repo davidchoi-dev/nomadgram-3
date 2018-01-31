b17562aa8f40cf67a7cbb54aac040ddd8b540e8d
Andy Woo <cheese.wo@gmail.com>
2018-02-01 07:56:50 +0900

    add valuese input Signupform

--- frontend/src/components/SignupForm/container.js
+++ frontend/src/components/SignupForm/container.js
@@ -1,7 +1,35 @@
-
-import React from "react";
+import React, { Component } from "react";
 import SignupForm from "./presenter";
 
-const Container = props => <SignupForm {...props} />;
+class Container extends Component{
+    state = {
+        email: "",
+        username: "",
+        password: ""
+    }
+
+    render(){
+
+        const {email, name, username, password} = this.state;
+        return <SignupForm emailValue={email}
+                    nameValue={name}
+                    usernameValue={username}
+                    passwordValue={password}
+        
+        />;
+
+
+    }
+    _handelInputChange = event => {
+        const { target: { value, name }} = event;
+        this.setState({
+            [name]: value
+        })
+        console.log(event);
+
+    }
+}
+
+
 
 export default Container;
--- frontend/src/components/SignupForm/presenter.js
+++ frontend/src/components/SignupForm/presenter.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Ionicon from "react-ionicons";
+import PropTypes from 'prop-types';
 import formStyles from "shared/formStyles.scss";
 
 
@@ -13,17 +14,32 @@ const SignupForm = props => (
       </button>
       <span className={formStyles.divider}>or</span>
       <form className={formStyles.form}>
-        <input type="email" placeholder="Email" className={formStyles.textInput} />
-        <input type="text" placeholder="Full Name" className={formStyles.textInput} />
+        <input 
+          type="email" 
+          placeholder="Email" 
+          className={formStyles.textInput} 
+          name="email"
+          value={props.emailValue}
+          />
+        <input type="text" 
+          placeholder="Full Name" 
+          className={formStyles.textInput} 
+          name="name"
+          value={props.nameValue}
+          />
         <input
-          type="username"
+          type="text"
           placeholder="Username"
           className={formStyles.textInput}
+          name="username"
+          value={props.usernameValue}
         />
         <input
           type="password"
           placeholder="Password"
           className={formStyles.textInput}
+          name="password"
+          value={props.passwordValue}
         />
         <input type="submit" value="Sign up" className={formStyles.button} />
       </form>
@@ -32,5 +48,17 @@ const SignupForm = props => (
       </p>
     </div>
   );
+
+  SignupForm.propTypes = {
+    emailValue: PropTypes.string.isRequired,
+    nameValue: PropTypes.string.isRequried,
+    usernameValue: PropTypes.string.isRequired,
+    passwordValue: PropTypes.string.isRequired,
+    handelInputChange: PropTypes.func.isRequired
+  }
+
+  SignupForm.contextType = {
+    t: PropTypes.func.isRequired
+  }
 
   export default SignupForm;
