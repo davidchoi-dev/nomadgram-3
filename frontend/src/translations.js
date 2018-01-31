1469136a03709fc00088428cbcb1e97188b2ad3e
Andy Woo <cheese.wo@gmail.com>
2018-01-31 15:01:57 +0900

    #3-29 React Context And Translatin the Footer

--- frontend/src/translations.js
+++ frontend/src/translations.js
@@ -1,6 +1,7 @@
 export const translations = {
     es: {
-        "Login in": "Inciar Sesion"
+        "Login in": "Inciar Sesion",
+        "About Us": "Sobre nosotro"
     },
     kr: {
         "Log in": "로그인"
