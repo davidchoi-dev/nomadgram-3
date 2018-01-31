933267a3981fb0680408ad6427232ddd44ed0ed9
Andy Woo <cheese.wo@gmail.com>
2018-01-31 11:37:20 +0900

    #3-27 Finishg Auth Component and the Auth Forms

--- frontend/src/components/AuthForms/index.js
+++ frontend/src/components/AuthForms/index.js
@@ -1,40 +1,53 @@
-import React from 'react';
+import React from "react";
 import Ionicon from "react-ionicons";
 import styles from "./styles.scss";
 
-
-
 export const LoginForm = props => (
-    <div>
-        <form>
-            <input type="text" placeholder="Username"/>
-            <input type="password" placeholder="Password" />
-            <input type="submit" value="log in" />
-        </form>
-        <span>or</span>
-        <span>
-            <Ionicon icon="logo-facebook" fontSize="20px" color="#385185" />
-            Log in with Facebook</span>
-        <span>Forgot password?</span>
-    </div>
-
-
+  <div className={styles.formComponent}>
+    <form className={styles.form}>
+      <input type="text" placeholder="Username" className={styles.textInput} />
+      <input
+        type="password"
+        placeholder="Password"
+        className={styles.textInput}
+      />
+      <input type="submit" value="Log in" className={styles.button} />
+    </form>
+    <span className={styles.divider}>or</span>
+    <span className={styles.facebookLink}>
+      <Ionicon icon="logo-facebook" fontSize="20px" color="#385185" /> Log in
+      with Facebook
+    </span>
+    <span className={styles.forgotLink}>Forgot password?</span>
+  </div>
 );
 
 export const SignupForm = props => (
-    <div>
-        <h3>Sign up to see photos and videos fom your friends.</h3>
-        <button>
-            {" "}
-            <Ionicon icon="logo-facebook" fontSize="20px" color="white" />Log in
-            with Facebook</button>
-        <span>or</span>
-        <form>
-            <input type="email" placeholder="Email"/>
-            <input type="text" placeholder="Full Name" />
-            <input type="username" placeholder="Username" />
-            <input type="password" placeholder="Password" />
-            <input type="submit" value="Sign up" />
-        </form>
-    </div>
+  <div className={styles.formComponent}>
+    <h3 className={styles.signupHeader}>
+    Sign up to see photos and videos from your friends.</h3>
+    <button className={styles.button}>
+      <Ionicon icon="logo-facebook" fontSize="20px" color="white" /> Log in with
+      Facebook
+    </button>
+    <span className={styles.divider}>or</span>
+    <form className={styles.form}>
+      <input type="email" placeholder="Email" className={styles.textInput} />
+      <input type="text" placeholder="Full Name" className={styles.textInput} />
+      <input
+        type="username"
+        placeholder="Username"
+        className={styles.textInput}
+      />
+      <input
+        type="password"
+        placeholder="Password"
+        className={styles.textInput}
+      />
+      <input type="submit" value="Sign up" className={styles.button} />
+    </form>
+    <p className={styles.terms}>
+      By signing up, you agree to our <span>Terms & Privacy Policy</span>.
+    </p>
+  </div>
 );
